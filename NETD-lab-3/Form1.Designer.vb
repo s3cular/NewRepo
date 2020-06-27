521bbf4274edf8a723884e64068316dd76a22def
Smit Mangroliya <smit.mangroliya@dcmail.ca>
2020-06-27 10:52:05 -0400

    Created Textbox, labels and button in the form.

--- NETD-lab-3/Form1.Designer.vb
+++ NETD-lab-3/Form1.Designer.vb
@@ -22,17 +22,212 @@ Partial Class frmAverageUnits
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblDays = New System.Windows.Forms.Label()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblEmployee1 = New System.Windows.Forms.Label()
+        Me.lblEmployee2 = New System.Windows.Forms.Label()
+        Me.lblEmployee3 = New System.Windows.Forms.Label()
+        Me.txtEmp1 = New System.Windows.Forms.TextBox()
+        Me.txtEmp2 = New System.Windows.Forms.TextBox()
+        Me.txtEmp3 = New System.Windows.Forms.TextBox()
+        Me.lblOutput1 = New System.Windows.Forms.Label()
+        Me.lblOutput2 = New System.Windows.Forms.Label()
+        Me.lblOutput3 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'lblDays
+        '
+        Me.lblDays.AutoSize = True
+        Me.lblDays.Location = New System.Drawing.Point(18, 15)
+        Me.lblDays.Name = "lblDays"
+        Me.lblDays.Size = New System.Drawing.Size(45, 17)
+        Me.lblDays.TabIndex = 0
+        Me.lblDays.Text = "Day 1"
+        '
+        'lblUnits
+        '
+        Me.lblUnits.AutoSize = True
+        Me.lblUnits.Location = New System.Drawing.Point(18, 45)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(44, 17)
+        Me.lblUnits.TabIndex = 1
+        Me.lblUnits.Text = "Units:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TextBox1.Location = New System.Drawing.Point(76, 45)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'lblEmployee1
+        '
+        Me.lblEmployee1.AutoSize = True
+        Me.lblEmployee1.Location = New System.Drawing.Point(30, 81)
+        Me.lblEmployee1.Name = "lblEmployee1"
+        Me.lblEmployee1.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmployee1.TabIndex = 3
+        Me.lblEmployee1.Text = "Employee 1"
+        '
+        'lblEmployee2
+        '
+        Me.lblEmployee2.AutoSize = True
+        Me.lblEmployee2.Location = New System.Drawing.Point(136, 81)
+        Me.lblEmployee2.Name = "lblEmployee2"
+        Me.lblEmployee2.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmployee2.TabIndex = 4
+        Me.lblEmployee2.Text = "Employee 2"
+        '
+        'lblEmployee3
+        '
+        Me.lblEmployee3.AutoSize = True
+        Me.lblEmployee3.Location = New System.Drawing.Point(242, 81)
+        Me.lblEmployee3.Name = "lblEmployee3"
+        Me.lblEmployee3.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmployee3.TabIndex = 5
+        Me.lblEmployee3.Text = "Employee 3"
+        '
+        'txtEmp1
+        '
+        Me.txtEmp1.Location = New System.Drawing.Point(21, 112)
+        Me.txtEmp1.Multiline = True
+        Me.txtEmp1.Name = "txtEmp1"
+        Me.txtEmp1.Size = New System.Drawing.Size(100, 170)
+        Me.txtEmp1.TabIndex = 6
+        '
+        'txtEmp2
+        '
+        Me.txtEmp2.Location = New System.Drawing.Point(127, 112)
+        Me.txtEmp2.Multiline = True
+        Me.txtEmp2.Name = "txtEmp2"
+        Me.txtEmp2.Size = New System.Drawing.Size(100, 171)
+        Me.txtEmp2.TabIndex = 7
+        '
+        'txtEmp3
+        '
+        Me.txtEmp3.Location = New System.Drawing.Point(233, 113)
+        Me.txtEmp3.Multiline = True
+        Me.txtEmp3.Name = "txtEmp3"
+        Me.txtEmp3.Size = New System.Drawing.Size(100, 170)
+        Me.txtEmp3.TabIndex = 8
+        '
+        'lblOutput1
+        '
+        Me.lblOutput1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblOutput1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput1.Location = New System.Drawing.Point(21, 290)
+        Me.lblOutput1.Name = "lblOutput1"
+        Me.lblOutput1.Size = New System.Drawing.Size(100, 23)
+        Me.lblOutput1.TabIndex = 9
+        '
+        'lblOutput2
+        '
+        Me.lblOutput2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput2.Location = New System.Drawing.Point(127, 290)
+        Me.lblOutput2.Name = "lblOutput2"
+        Me.lblOutput2.Size = New System.Drawing.Size(100, 23)
+        Me.lblOutput2.TabIndex = 10
+        '
+        'lblOutput3
+        '
+        Me.lblOutput3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput3.Location = New System.Drawing.Point(233, 290)
+        Me.lblOutput3.Name = "lblOutput3"
+        Me.lblOutput3.Size = New System.Drawing.Size(100, 23)
+        Me.lblOutput3.TabIndex = 11
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(21, 313)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(312, 29)
+        Me.lblResult.TabIndex = 12
+        '
+        'btnEnter
+        '
+        Me.btnEnter.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnEnter.Location = New System.Drawing.Point(20, 345)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(101, 35)
+        Me.btnEnter.TabIndex = 13
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnReset.Location = New System.Drawing.Point(127, 345)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(100, 35)
+        Me.btnReset.TabIndex = 14
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnExit.Location = New System.Drawing.Point(233, 345)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 35)
+        Me.btnExit.TabIndex = 15
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = False
         '
         'frmAverageUnits
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(426, 397)
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(343, 391)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblOutput3)
+        Me.Controls.Add(Me.lblOutput2)
+        Me.Controls.Add(Me.lblOutput1)
+        Me.Controls.Add(Me.txtEmp3)
+        Me.Controls.Add(Me.txtEmp2)
+        Me.Controls.Add(Me.txtEmp1)
+        Me.Controls.Add(Me.lblEmployee3)
+        Me.Controls.Add(Me.lblEmployee2)
+        Me.Controls.Add(Me.lblEmployee1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.lblDays)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "frmAverageUnits"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Average Units Shipped"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblDays As Label
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents lblEmployee1 As Label
+    Friend WithEvents lblEmployee2 As Label
+    Friend WithEvents lblEmployee3 As Label
+    Friend WithEvents txtEmp1 As TextBox
+    Friend WithEvents txtEmp2 As TextBox
+    Friend WithEvents txtEmp3 As TextBox
+    Friend WithEvents lblOutput1 As Label
+    Friend WithEvents lblOutput2 As Label
+    Friend WithEvents lblOutput3 As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
 End Class
